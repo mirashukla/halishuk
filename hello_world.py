7ad9b582c125eb517cf0046df9ea1921a837aaa4
Baileyh19199 <150626701+Baileyh19199@users.noreply.github.com>
2023-11-23 21:58:31 +0000

    Added get cwd

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import os
 
 # Create the main application window
 root = tk.Tk()
@@ -6,7 +7,7 @@ root.title("Image Viewer")
 
 # Open the image file in PhotoImage format
 # Note: PhotoImage has limited support for PNG; check if your image is displayed correctly
-image_path = "black_hole.png"  # Replace with the path to your PNG image
+image_path = str(os.getcwd()) + "/black_hole.png"  # Replace with the path to your PNG image
 tk_image = tk.PhotoImage(file=image_path)
 
 # Create a label to display the image
